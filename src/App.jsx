bde7098f136430c20c04d94a24a31dd2e22e772f
faridrawing <muhammad.farid.yamin@gmail.com>
2024-08-17 18:19:44 +0700

    chore: setup configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <BrowserRouter></BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header className="bg-blue-400">Aku header</header>;
+};
+
+export default Header;
