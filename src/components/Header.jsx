cf2ab4bbb738fc9fd086088dd757429b8e77196c
faridrawing <muhammad.farid.yamin@gmail.com>
2024-08-17 21:00:13 +0700

    feat: add navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,56 @@
 const Header = () => {
-  return <header className="bg-blue-400">Aku header</header>;
+  const currentUrl = window.location.href.split("/").pop();
+
+  return (
+    <header className="container mx-auto bg-white px-8 py-6 flex flex-col gap-4 sm:flex-row sm:justify-between">
+      <a
+        href="./"
+        className="font-poppins font-bold uppercase text-sm tracking-widest text-black"
+      >
+        faridreaming.
+      </a>
+      <nav>
+        <ul className="flex text-xs justify-between items-center h-full gap-4 whitespace-nowrap">
+          <li>
+            <a
+              href="./"
+              className={currentUrl === "" ? "text-blue-500" : undefined}
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="./cv"
+              className={currentUrl === "cv" ? "text-blue-500" : undefined}
+            >
+              CV
+            </a>
+          </li>
+          <li>
+            <a
+              href="./portfolio"
+              className={
+                currentUrl === "portfolio" ? "text-blue-500" : undefined
+              }
+            >
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a
+              href="./certifications"
+              className={
+                currentUrl === "certifications" ? "text-blue-500" : undefined
+              }
+            >
+              Certifications
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
