43195149ed4eb83029309edc7c5fc606aac14709
faridrawing <muhammad.farid.yamin@gmail.com>
2024-08-18 11:09:50 +0700

    fix: fix the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,27 @@
-const Header = () => {
+const Header = ({ brandName }) => {
   const currentUrl = window.location.href.split("/").pop();
 
   return (
-    <header className="container mx-auto bg-white px-8 py-6 flex flex-col gap-4 sm:flex-row sm:justify-between">
-      <a
-        href="./"
-        className="font-poppins font-bold uppercase text-sm tracking-widest text-black"
-      >
-        faridreaming.
+    <header className="container max-w-4xl mx-auto bg-white px-8 py-10 flex flex-col gap-4 sm:flex-row sm:justify-between">
+      <a href="./" className="font-poppins font-bold uppercase text-lg tracking-widest text-black">
+        {`${brandName}.`}
       </a>
       <nav>
         <ul className="flex text-xs justify-between items-center h-full gap-4 whitespace-nowrap">
           <li>
-            <a
-              href="./"
-              className={currentUrl === "" ? "text-blue-500" : undefined}
-            >
+            <a href="./" className={`hover:text-blue-500 ${currentUrl === "" ? "text-blue-500" : undefined}`}>
               Home
             </a>
           </li>
           <li>
-            <a
-              href="./cv"
-              className={currentUrl === "cv" ? "text-blue-500" : undefined}
-            >
+            <a href="./cv" className={`hover:text-blue-500 ${currentUrl === "cv" ? "text-blue-500" : undefined}`}>
               CV
             </a>
           </li>
           <li>
             <a
               href="./portfolio"
-              className={
-                currentUrl === "portfolio" ? "text-blue-500" : undefined
-              }
+              className={`hover:text-blue-500 ${currentUrl === "portfolio" ? "text-blue-500" : undefined}`}
             >
               Portfolio
             </a>
@@ -40,9 +29,7 @@ const Header = () => {
           <li>
             <a
               href="./certifications"
-              className={
-                currentUrl === "certifications" ? "text-blue-500" : undefined
-              }
+              className={`hover:text-blue-500 ${currentUrl === "certifications" ? "text-blue-500" : undefined}`}
             >
               Certifications
             </a>
