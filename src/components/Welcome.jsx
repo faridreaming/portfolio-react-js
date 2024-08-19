763bbe36cd445af6e785df69702c28f07795897c
faridrawing <muhammad.farid.yamin@gmail.com>
2024-08-20 02:30:01 +0700

    feat: create welcome component

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+const Welcome = () => {
+  return (
+    <div className="container mx-auto max-w-4xl h-[60svh] flex items-center px-8">
+      <h1 className="font-semibold font-poppins text-3xl sm:text-4xl text-primary tracking-wider [word-spacing:0.5rem] leading-normal">
+        Hi, I'm Farid! <br />
+        I'm a software engineer, and I love to build things with code.
+      </h1>
+    </div>
+  );
+};
+
+export default Welcome;
