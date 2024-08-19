5f65d8da2bd939ad373a1f38f69a8758df80f8fb
faridrawing <muhammad.farid.yamin@gmail.com>
2024-08-20 02:30:24 +0700

    feat: create hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,10 @@ import { hero } from "../assets/images";
 
 const Hero = () => {
   return (
-    <div className="w-full flex flex-col sm:flex-row h-[640px]">
-      <img src={hero} alt="Hero image" className="object-cover object-center w-full max-sm:aspect-square sm:w-1/2" />
-      <div className="bg-primary px-8 py-16 sm:py-28 sm:px-16 sm:w-1/2">
-        <div className="bg-white p-8 text-sm relative hero-text-box left-2.5 bottom-[5px] sm:h-full sm:flex sm:flex-col sm:justify-between sm:max-w-md sm:mx-auto overflow-y-auto">
+    <div className="w-full flex flex-col sm:flex-row sm:h-[640px]">
+      <img src={hero} alt="Hero image" className="object-cover object-top w-full max-sm:aspect-square sm:w-1/2" />
+      <div className="bg-primary px-8 py-16 lg:py-28 lg:px-16 sm:w-1/2">
+        <div className="bg-white p-8 text-sm sm:text-md relative hero-text-box left-2.5 bottom-[5px] sm:h-full sm:flex sm:flex-col sm:justify-between sm:max-w-sm sm:mx-auto overflow-y-auto">
           <div className="space-y-4">
             <p>My hobbies include reading, writing, and playing video games. I eat food and drink fluid water.</p>
             <p>
