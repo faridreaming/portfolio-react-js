fc9e5b5595a669d8ea3530e79059fcc6bd351fa1
faridrawing <muhammad.farid.yamin@gmail.com>
2024-08-20 19:03:05 +0700

    feat: add home page to the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Home } from "./pages";
+
+const Container = ({ children }) => {
+  return <div className="container mx-auto max-w-4xl">{children}</div>;
+};
+
+const brand = "Faridreaming";
+const pageTitle = (pageName) => `${pageName} | Farid's Website`;
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <BrowserRouter></BrowserRouter>
+      <Header brandName={brand} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home pageTitle={pageTitle("Home")} />} />
+          <Route path="/cv" element={""} />
+          <Route path="/portfolio" element={""} />
+          <Route path="/certifications" element={""} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 };
