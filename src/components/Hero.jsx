979c4ba9f142b45eaed5bee8c7796eb8f938407e
faridrawing <muhammad.farid.yamin@gmail.com>
2024-08-20 21:21:27 +0700

    chore: format the code with prettier

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,10 @@ import { hero } from "../assets/images";
 
 const Hero = () => {
   return (
-    <div className="w-full flex flex-col sm:flex-row sm:h-[640px]">
-      <img src={hero} alt="Hero image" className="object-cover object-top w-full max-sm:aspect-square sm:w-1/2" />
-      <div className="bg-primary px-8 py-16 lg:py-28 lg:px-16 sm:w-1/2">
-        <div className="bg-white p-8 text-sm sm:text-md relative hero-text-box left-2.5 bottom-[5px] sm:h-full sm:flex sm:flex-col sm:justify-between sm:max-w-sm sm:mx-auto overflow-y-auto">
+    <div className="flex w-full flex-col sm:h-[640px] sm:flex-row">
+      <img src={hero} alt="Hero image" className="w-full object-cover object-top max-sm:aspect-square sm:w-1/2" />
+      <div className="bg-primary px-8 py-16 sm:w-1/2 lg:px-16 lg:py-28">
+        <div className="sm:text-md hero-text-box relative bottom-[5px] left-2.5 overflow-y-auto bg-white p-8 text-sm sm:mx-auto sm:flex sm:h-full sm:max-w-sm sm:flex-col sm:justify-between">
           <div className="space-y-4">
             <p>My hobbies include reading, writing, and playing video games. I eat food and drink fluid water.</p>
             <p>
@@ -14,7 +14,7 @@ const Hero = () => {
             </p>
             <p>I'm currently learning about web development, and I'm excited to share my projects with you.</p>
           </div>
-          <a href="/cv" className="block mt-8 bg-black text-white text-center px-8 py-2 w-full sm:w-fit">
+          <a href="/cv" className="mt-8 block w-full bg-black px-8 py-2 text-center text-white sm:w-fit">
             Check out my CV
           </a>
         </div>
--- src/components/WhatIDo.jsx
+++ src/components/WhatIDo.jsx
@@ -1,27 +1,27 @@
 const WhatIDo = () => {
   return (
     <div className="container mx-auto max-w-4xl px-8 py-10 sm:p-20">
-      <h2 className="font-bold font-poppins text-2xl uppercase tracking-widest text-black mb-10 sm:mb-20">What I Do</h2>
-      <div className="text-sm sm:text-md grid gap-10 sm:gap-y-20 sm:grid-cols-2">
+      <h2 className="mb-10 font-poppins text-2xl font-bold uppercase tracking-widest text-black sm:mb-20">What I Do</h2>
+      <div className="sm:text-md grid gap-10 text-sm sm:grid-cols-2 sm:gap-y-20">
         <div>
-          <h3 className="font-medium font-poppins text-black mb-4">Software Development</h3>
-          <ul className="list-disc list-outside px-8 leading-relaxed">
+          <h3 className="mb-4 font-poppins font-medium text-black">Software Development</h3>
+          <ul className="list-outside list-disc px-8 leading-relaxed">
             <li>Creating a software application from scratch using the latest technologies.</li>
             <li>Maintaining and updating existing software applications.</li>
             <li>Debugging and troubleshooting software applications.</li>
           </ul>
         </div>
         <div className="sm:row-span-2">
-          <h3 className="font-medium font-poppins text-black mb-4">Web Development</h3>
-          <ul className="list-disc list-outside px-8 leading-relaxed">
+          <h3 className="mb-4 font-poppins font-medium text-black">Web Development</h3>
+          <ul className="list-outside list-disc px-8 leading-relaxed">
             <li>Creating a website from scratch using the latest technologies.</li>
             <li>Optimizing websites for speed and performance.</li>
             <li>Creating responsive websites that work on all devices.</li>
           </ul>
         </div>
         <div>
-          <h3 className="font-medium font-poppins text-black mb-4">UI/UX Design</h3>
-          <ul className="list-disc list-outside px-8 leading-relaxed">
+          <h3 className="mb-4 font-poppins font-medium text-black">UI/UX Design</h3>
+          <ul className="list-outside list-disc px-8 leading-relaxed">
             <li>Creating wireframes and mockups for websites and applications.</li>
             <li>Designing user-friendly interfaces that are easy to navigate.</li>
             <li>Creating visually appealing designs that engage users.</li>
