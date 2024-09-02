bb353ba85c4c8331d7b19206d0268da5e7e78e76
faridreaming <muhammad.farid.yamin@gmail.com>
2024-09-03 06:39:51 +0700

    feat: add open document feature

--- src/components/PDFViewer.jsx
+++ src/components/PDFViewer.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
+import { eye } from "../assets/icons";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 
 const PDFViewer = ({ fileUrl }) => {
@@ -23,8 +24,18 @@ const PDFViewer = ({ fileUrl }) => {
     };
   }, []);
 
+  const openFile = () => {
+    window.open(fileUrl, "_blank");
+  };
+
   return (
-    <div className="shadow-none sm:shadow">
+    <div className="relative shadow-none sm:shadow">
+      <a
+        className="absolute right-0 top-0 z-40 mr-8 flex cursor-pointer items-center justify-center bg-primary p-1 transition hover:opacity-50 sm:mr-0 sm:p-2"
+        onClick={openFile}
+      >
+        <img src={eye} alt="Open document" className="w-5 invert sm:w-6" />
+      </a>
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
         <Viewer fileUrl={fileUrl} key={key} />
       </Worker>
