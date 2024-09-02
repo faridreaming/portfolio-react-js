0c0a9ad0a527ec4e6c5c68773b2ded61ecea3c0c
faridreaming <muhammad.farid.yamin@gmail.com>
2024-09-03 06:40:14 +0700

    feat: add open document feature

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,6 @@
 import github from "./github.svg";
 import linkedin from "./linkedin.svg";
 import instagram from "./instagram.svg";
+import eye from "./eye.svg";
 
-export { github, linkedin, instagram };
+export { github, linkedin, instagram, eye };
