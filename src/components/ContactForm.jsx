0fe2d88d1bb1bdb25cc2d573649f5b61980182b5
faridreaming <muhammad.farid.yamin@gmail.com>
2024-09-02 19:40:46 +0700

    fix: fix the contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,7 +6,7 @@ const ContactForm = () => {
         name="name"
         id="name"
         placeholder="Name"
-        className="w-full px-3 py-2 outline-none focus:bg-transparent focus:text-white focus:ring-1 focus:ring-white"
+        className="w-full bg-black/10 px-3 py-2 text-white outline-none placeholder:text-white/65 hover:bg-black/20 focus:bg-white focus:text-slate-500 focus:placeholder:text-slate-500/50"
         autoComplete="off"
       />
       <input
@@ -14,7 +14,7 @@ const ContactForm = () => {
         name="email"
         id="email"
         placeholder="Email"
-        className="w-full px-3 py-2 outline-none focus:bg-transparent focus:text-white focus:ring-1 focus:ring-white"
+        className="w-full bg-black/10 px-3 py-2 text-white outline-none placeholder:text-white/65 hover:bg-black/20 focus:bg-white focus:text-slate-500 focus:placeholder:text-slate-500/50"
         autoComplete="off"
       />
       <input
@@ -24,18 +24,18 @@ const ContactForm = () => {
         name="phone"
         id="phone"
         placeholder="Phone"
-        className="w-full px-3 py-2 outline-none focus:bg-transparent focus:text-white focus:ring-1 focus:ring-white"
+        className="w-full bg-black/10 px-3 py-2 text-white outline-none placeholder:text-white/65 hover:bg-black/20 focus:bg-white focus:text-slate-500 focus:placeholder:text-slate-500/50"
         autoComplete="off"
       />
       <textarea
         name="message"
         id="message"
         placeholder="Type your message here"
-        className="h-24 w-full px-3 py-2 outline-none focus:bg-transparent focus:text-white focus:ring-1 focus:ring-white"
+        className="h-24 w-full resize-none bg-black/10 px-3 py-2 text-white outline-none placeholder:text-white/65 hover:bg-black/20 focus:bg-white focus:text-slate-500 focus:placeholder:text-slate-500/50"
       ></textarea>
       <button
         type="submit"
-        className="w-1/2 self-end border bg-transparent py-1 font-semibold text-white outline-none transition hover:bg-white hover:text-primary focus:bg-white focus:text-primary sm:w-fit sm:px-6"
+        className="box-border w-1/2 self-end bg-primary py-1 font-semibold text-white outline outline-2 outline-white transition hover:bg-white hover:text-primary focus:bg-white focus:text-primary focus:outline-offset-2 sm:w-fit sm:px-6"
       >
         Submit
       </button>
